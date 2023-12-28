7d6b46a6cb2319ef19c54b20dbf665a33ddf028d
whiskascats <whiskascats@users.noreply.github.com>
2023-12-28 10:00:43 +0000

    deploy: 8fa1a176a808bf6e7b37530ea73b590dc1cbd085

--- assets/index-Bd3G-s77.js
+++
@@ -1,1 +0,0 @@
-import{y as e}from"./index-giqcfcQP.js";const r=e("score",{state:()=>({score:0,bestScore:0})});export{r as u};
---
+++ assets/index-v6osc8Fv.js
@@ -0,0 +1,1 @@
+import{y as e}from"./index-4eSGHWeu.js";const r=e("score",{state:()=>({score:0,bestScore:0})});export{r as u};
